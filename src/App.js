401dd1760e8e7fc2915f0d8b456a5412f153089b
SUprem Nandal <suprem.nandal@gmail.com>
2020-04-05 01:33:52 +0530

    form sumit react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
+import axios from 'axios';
 
 // local import here.
 import './static/css/style.css'
 
 class ReactFormLabel extends React.Component {
-    constructor(props) {
-     super(props)
-    }
-   
+
     render() {
      return(
       <label htmlFor={this.props.htmlFor}>{this.props.title}</label>
@@ -17,118 +15,95 @@ class ReactFormLabel extends React.Component {
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
-      
-        this.state = {
-         name: '',
+
+    state = {
          email: '',
-         subject: '',
-         message: ''
+         phone: '',
+         ocd: '',
         }
       
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-       }
+        handleChange = e => {
+            this.setState({[e.target.name]: e.target.value})
+          }
       
-       handleChange = (e) => {
-        let newState = {}
-      
-        newState[e.target.name] = e.target.value
-      
-        this.setState(newState)
-       }
-      
-      
-       handleSubmit = (e, message) => {
+        onFormSubmit = (e) => {
         e.preventDefault()
       
         let formData = {
-         formSender: this.state.name,
-         formEmail: this.state.email,
-         formSubject: this.state.subject,
-         formMessage: this.state.message
+         email: this.state.email,
+         phone: this.state.phone,
+         ocd: this.state.ocd,
+         
         }
       
-        if (formData.formSender.length < 1 || formData.formEmail.length < 1 || formData.formSubject.length < 1 || formData.formMessage.length < 1) {
-         return false
-        }
-      
-        // $.ajax({
-        //  url: '/some/url',
-        //  dataType: 'json',
-        //  type: 'POST',
-        //  data: formData,
-        //  success: function(data) {
-        //   if (confirm('Thank you for your message. Can I erase the form?')) {
-        //     this.setState({
-        //      firstName: '',
-        //      lastName: '',
-        //      email: '',
-        //      subject: '',
-        //      message: ''
-        //     })
-        //   }
-        //  },
-        //  error: function(xhr, status, err) {
-        //   console.error(status, err.toString())
-        //   alert('There was some problem with sending your message.')
-        //  }
-        // })
-      
-        this.setState({
-         firstName: '',
-         lastName: '',
-         email: '',
-         subject: '',
-         message: ''
+        if (formData.phone.length < 10) {
+            return alert('phone must be 10 digits')
+           }
+   
+        axios.post('https://mypodcst.herokuapp.com/email/create/', formData)
+        .then(response => {
+            console.log(response)
+            if (response.status === 201){
+                this.setState({
+                    email: '',
+                    phone: '',
+                    ocd: '',    
+                }) 
+            //    this.props.history.push('/moneyform'); 
+            }
         })
+        .catch(err => {
+            console.log(err)
+            return alert('Mobile Or Email Exist')
+        });  
        }
+
     render() {
         return (
             <React.Fragment>
-            <header>
-                <form className='react-form' onSubmit={this.handleSubmit}>
-                <h1>Access Podcast</h1>
-    
+            <header className='header'>
+                <div className='header__container'>
+                <div className='header__text-box'>
+                    <h3 className='heading-tertiary'>
 
-    
-                        <fieldset className='form-group'>
-                        <ReactFormLabel htmlFor='formEmail' title='Email:' />
-    
-                        <input id='formEmail' className='form-input' name='email' type='email' required onChange={this.handleChange} value={this.state.email} />
-                        </fieldset>
-    
-                        <fieldset className='form-group'>
-                        <ReactFormLabel htmlFor='formSubject' title='Mobile:'/>
-    
-                        <input id='formSubject' className='form-input' name='subject' type='text' required onChange={this.handleChange} value={this.state.subject} />
-                        </fieldset>
-    
-                        <fieldset className='form-group'>
-                        <ReactFormLabel htmlFor='formMessage' title='Select Type:' />
+                 
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </h3>
+                </div>
+                <div className='header__form-box'>
+                    <form className='react-form' onSubmit={(event) => this.onFormSubmit(event)}>
 
-                            <select id="cars">
-                            <option value="1">CONTAMINATION</option>
-                            <option value="2">RELIGIOUS</option>
-                            <option value="3">SOCIAL</option>
-                            <option value="4">OTHER</option>
-                            </select>
-                        </fieldset>
+                            <h1 className='form-title'>Access Podcast</h1>
 
-                        <fieldset className='form-group'>
-                        <ReactFormLabel htmlFor='formMessage' title='Message:' />
-    
-                        <textarea id='formMessage' className='form-textarea' name='message' onChange={this.handleChange}></textarea>
-                        </fieldset>
+                            <fieldset className='form-group'>
+                            <ReactFormLabel htmlFor='formEmail' title='Email:' />
+        
+                            <input id='formEmail' className='form-input' name='email' type='email' required onChange={this.handleChange} value={this.state.email} />
+                            </fieldset>
+        
+                            <fieldset className='form-group'>
+                            <ReactFormLabel htmlFor='phone' title='Mobile:'/>
+        
+                            <input id='phone' className='form-input' name='phone' type='text' required onChange={this.handleChange} value={this.state.phone} />
+                            </fieldset>
+        
+                            <fieldset className='form-group'>
+                            <ReactFormLabel htmlFor='formMessage' title='Select Type:' />
 
+                                <select id="cars">
+                                <option value="1">CONTAMINATION</option>
+                                <option value="2">RELIGIOUS</option>
+                                <option value="3">SOCIAL</option>
+                                <option value="4">OTHER</option>
+                                </select>
+                            </fieldset>
 
-
-    
-                        <div className='form-group'>
-                        <input id='formButton' className='btn' type='submit' placeholder='Send message' />
-                        </div>
-                </form>
+                            <div className='form-group'>
+                            <input id='formButton' className='btn' type='submit' placeholder='Send message' />
+                            </div>
+                    </form>
+                </div>
+                </div>
             </header>
             </React.Fragment>
         )
